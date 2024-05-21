4b1ee4fb1baa1f7172a166ed3f0ebc75520d083f
ppedvLukasK <lukask@ppedv.de>
2024-05-21 16:24:12 +0200

    M004

---
+++ M004/DoubleToBrushConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace M004;
+
+public class DoubleToBrushConverter : IValueConverter
+{
+	/// <summary>
+	/// Quelle -> Ziel
+	/// Slider -> Rectangle
+	/// 
+	/// value: double
+	/// return: Brush
+	/// </summary>
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		double sliderValue = (double)value;
+		byte color = (byte) sliderValue;
+		SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(255, color, color, color));
+		return brush;
+	}
+
+	/// <summary>
+	/// Ziel -> Quelle
+	/// Rectangle -> Slider
+	/// 
+	/// value: Brush
+	/// return: double
+	/// 
+	/// (Kann nicht passieren)
+	/// </summary>
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return 0;
+	}
+}
---
+++ M004/FourValueToMarginConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M004;
+
+public class FourValueToMarginConverter : IMultiValueConverter
+{
+	/// <summary>
+	/// values enthält hier vier Doubles
+	/// LSlider, TSlider, RSlider, BSlider
+	/// </summary>
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		//double[] doubles = values.OfType<double>().ToArray();
+		double[] doubles = new double[values.Length];
+		for (int i = 0; i < values.Length; i++)
+		{
+			doubles[i] = (double) values[i];
+		}
+		return new Thickness(doubles[0], doubles[1], doubles[2], doubles[3]);
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => Array.Empty<object>();
+}
