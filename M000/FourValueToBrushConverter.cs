3f1f476fb28845cf7bca1ec00bb4a7cf226860e3
ppedvLukasK <lukask@ppedv.de>
2024-05-22 10:59:33 +0200

    M005

---
+++ M000/FourValueToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace M000;
+
+public class FourValueToBrushConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		byte[] bytes = values
+			.OfType<double>()
+			.Select(e => (byte) e)
+			.ToArray();
+		return new SolidColorBrush(Color.FromArgb(bytes[3], bytes[0], bytes[1], bytes[2]));
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ M005/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace M005;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+	private int zaehler = 10;
+
+	public int Zaehler
+	{
+		get => zaehler;
+		set
+		{
+			zaehler = value;
+			Notify(nameof(Zaehler));
+		}
+	}
+
+	public ObservableCollection<int> Zahlen { get; set; } = new();
+
+	public BindableProperty<int> Zaehler2 { get; set; } = new();
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		//this.DataContext = this;
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Zaehler++;
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		Zahlen.Add(Random.Shared.Next());
+	}
+
+	private void Button_Click_2(object sender, RoutedEventArgs e)
+	{
+		Zaehler2.Value++;
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	public void Notify(string propertyName) =>
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+	//Alternative:
+	//public void Notify([CallerMemberName] string propertyName = "") =>
+	//	PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
