8e9ba9bb8207f9678e7b6131b35cc3bc29d15cd2
ppedvLukasK <lukask@ppedv.de>
2024-05-22 12:53:05 +0200

    M005

---
+++ M000/GenderConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M000;
+
+public class GenderConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		//Quelle -> Ziel
+		//Person -> RB
+		//Geschlecht Property -> IsChecked
+		//Rückgabewert: bool
+
+		//value: Wert im der Person Objekt
+		//parameter: Parameter vom RadioButton
+		return (Geschlecht) value == (Geschlecht) parameter;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		//Ziel -> Quelle
+		//RB -> Person
+		//IsChecked -> Geschlecht Property
+		//Rückgabewert: Geschlecht
+
+		//value: IsChecked
+		//parameter: Parameter vom RadioButton
+		if ((bool) value == true)
+			return parameter;
+		return Binding.DoNothing;
+	}
+}
